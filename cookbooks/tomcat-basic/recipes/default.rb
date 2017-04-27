d2d7dd7db85b291cf530c2e88d3d5cd69c522243
Charlie Costello <c.costello@livelinktechnology.net>
2017-04-27 10:10:07 +0100

    Updated

--- cookbooks/tomcat-basic/recipes/default.rb
+++ cookbooks/tomcat-basic/recipes/default.rb
@@ -4,23 +4,13 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-group 'jenkins'
+include_recipe 'java'
 
-user 'jenkins' do
-  gid 'jenkins'
+tomcat_install 'tomcat' do
+  version '8.0.36'
 end
 
-application 'jenkins' do
-  path         '/usr/local/jenkins'
-  owner        'jenkins'
-  group        'jenkins'
-  repository   'http://mirrors.jenkins-ci.org/war/latest/jenkins.war'
-  revision     '6facd94e958ecf68ffd28be371b5efcb5584c885b5f32a906e477f5f62bdb518-1'
-    scm_provider Chef::Provider::RemoteFile::Deploy
-
-  tomcat
-end
-
-file '/tmp/cjtest' do
-  content 'Hello World! CJ'
+tomcat_service 'tomcat' do
+  action :start
+#  env_vars [{ 'JRE_HOME' => '/usr/lib/jvm/default-java' }]
 end
