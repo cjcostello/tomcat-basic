87e252b652d2520c6aa01f6d474213a818777d51
Charlie Costello <c.costello@livelinktechnology.net>
2017-04-27 12:49:35 +0100

    update

--- cookbooks/tomcat-basic/recipes/default.rb
+++ cookbooks/tomcat-basic/recipes/default.rb
@@ -12,17 +12,20 @@ tomcat_install 'cj' do
   version '8.0.36'
 end
 
-execute 'set jre home' do
-  command 'echo "JRE_HOME=/usr/lib/jvm/default-java/jre" > /etc/environment'
-end
+#execute 'set jre home' do
+#  command 'echo "JRE_HOME=/usr/lib/jvm/default-java/jre" > /etc/environment'
+#end
+#
+#execute 'set java home' do 
+#  'echo JAVA_HOME=/usr/lib/jvm/default-java > /etc/environment'
+#end
+#
+#execute 'refresh environment' do
+#  command 'source /etc/environment'
+#end
 
-execute 'set java home' do 
-  'echo JAVA_HOME=/usr/lib/jvm/default-java > /etc/environment'
-end
-
-execute 'refresh environment' do
-  command 'source /etc/environment'
-end
+ENV['JAVA_HOME'] = '/usr/lib/jvm/default-java'
+ENV['JRE_HOME'] = '/usr/lib/jvm/default-java/jre'
 
 tomcat_service 'cj' do
   action :start
