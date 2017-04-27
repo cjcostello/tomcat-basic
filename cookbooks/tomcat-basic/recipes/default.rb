85749f0ba9c75ffcc3a0aaf66bd4dd6bdcfdafc9
Charlie Costello <c.costello@livelinktechnology.net>
2017-04-27 09:40:36 +0100

    ::Update tomcat-basic cookbook and deps

--- cookbooks/tomcat-basic/recipes/default.rb
+++ cookbooks/tomcat-basic/recipes/default.rb
@@ -3,3 +3,24 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+group 'jenkins'
+
+user 'jenkins' do
+  gid 'jenkins'
+end
+
+application 'jenkins' do
+  path         '/usr/local/jenkins'
+  owner        'jenkins'
+  group        'jenkins'
+  repository   'http://mirrors.jenkins-ci.org/war/latest/jenkins.war'
+  revision     '6facd94e958ecf68ffd28be371b5efcb5584c885b5f32a906e477f5f62bdb518-1'
+    scm_provider Chef::Provider::RemoteFile::Deploy
+
+  tomcat
+end
+
+file '/tmp/cjtest' do
+  content 'Hello World! CJ'
+end
